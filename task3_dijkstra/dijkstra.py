2aed9020cfdec1dbc47473310b7598c978851b92
Larimar4you <Zajanezlaja@gmail.com>
2025-12-21 20:08:43 +0100

    PART

--- task3_dijkstra/dijkstra.py
+++ task3_dijkstra/dijkstra.py
@@ -1,39 +1,55 @@
 import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Створення власного вагового графа
+G = nx.Graph()
+G.add_edge("A", "B", weight=4)
+G.add_edge("A", "C", weight=2)
+G.add_edge("B", "C", weight=1)
+G.add_edge("B", "D", weight=5)
+G.add_edge("C", "D", weight=8)
+G.add_edge("C", "E", weight=10)
+G.add_edge("D", "E", weight=2)
 
 
+# Реалізація алгоритму Дейкстри
 def dijkstra(graph, start):
+    shortest_paths = {vertex: float("inf") for vertex in graph}
+    shortest_paths[start] = 0
+    priority_queue = [(0, start)]  # (distance, vertex)
 
-    distances = {v: float("inf") for v in graph}
-    distances[start] = 0
+    while priority_queue:
+        current_dist, current_vertex = heapq.heappop(priority_queue)
 
-    heap = [(0, start)]  # (distance, vertex)
-
-    while heap:
-        current_dist, current_vertex = heapq.heappop(heap)
-
-        if current_dist > distances[current_vertex]:
+        if current_dist > shortest_paths[current_vertex]:
             continue
 
-        for neighbor, weight in graph[current_vertex]:
+        for neighbor, attrs in graph[current_vertex].items():
+            weight = attrs["weight"]
             distance = current_dist + weight
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(heap, (distance, neighbor))
+            if distance < shortest_paths[neighbor]:
+                shortest_paths[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
 
-    return distances
+    return shortest_paths
 
 
-graph = {
-    "A": [("B", 4), ("C", 2)],
-    "B": [("A", 4), ("C", 1), ("D", 5)],
-    "C": [("A", 2), ("B", 1), ("D", 8), ("E", 10)],
-    "D": [("B", 5), ("C", 8), ("E", 2)],
-    "E": [("C", 10), ("D", 2)],
-}
-
+# Використання алгоритму Дейкстри
 start_vertex = "A"
-shortest_paths = dijkstra(graph, start_vertex)
+shortest_paths = dijkstra(G, start_vertex)
 
 for vertex, distance in shortest_paths.items():
     print(f"Відстань від {start_vertex} до {vertex}: {distance}")
+
+# Візуалізація графа
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, node_size=700)
+nx.draw_networkx_edges(G, pos, width=2)
+edge_labels = nx.get_edge_attributes(G, "weight")
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+
+plt.axis("off")
+plt.show()
